165f259665455643c1505c0ec0252a2ec4900628
aishwaryapaim <98397053+aishwaryapaim@users.noreply.github.com>
2022-01-29 18:13:47 +0530

    Update final table.sql

---
+++ models/Interview/final table.sql
@@ -0,0 +1,4 @@
+select a.*,b.* from INTERVIEW_DB.PLAYGROUND_aishwarya_pai.COVID_19_INDONESIA_lOCATIONS a 
+INNER JOIN INTERVIEW_DB.PLAYGROUND_aishwarya_pai.COVID_19_INDONESIA_CoviD_Cases b
+ON a.Latitude=b.Latitude
+AND a.Longitude=b.Longitude;
